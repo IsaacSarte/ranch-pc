9c0202fac9ad706513a60a2024d54ca2bc335c65
IsaacSarte <sarteisaac09@gmail.com>
2023-04-01 16:08:17 +0800

    added initial home component

--- components/Landing/Home.jsx
+++ components/Landing/Home.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import {
+  Flex,
+  Box,
+  Text,
+} from '@chakra-ui/react'
+
+import { motion } from 'framer-motion'
 
 const Home = () => {
   return (
     <div className="home__container">
-      
+      <Box>
+        <Flex direction="column" alignItems="center">
+          <motion.div
+            initial={{ opacity: 0, x: '-10vw' }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1.2, delay: 0.5 }}
+          >
+            <Text className="home-title title-1">
+              Future <br /> <Text className="home-title title-2">Computer</Text>
+            </Text>
+          </motion.div>
+        </Flex>
+
+        <Flex direction="column" alignItems="center">
+          <motion.div
+            initial={{ opacity: 0, x: '-10vw' }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1.2, delay: 1.5 }}
+          >
+            <Text className="home-details details-1">
+              Build your computer needs that satisfies you with Ranch PC <br />
+              <Text className="home-details details-2">Services including personal computer, business computer, <br />
+                <Text className="home-details details-3">and other computer needs.</Text>
+              </Text>
+            </Text>
+          </motion.div>
+        </Flex>
+      </Box>
     </div>
   )
 }
--- components/Landing/Navbar.jsx
+++ components/Landing/Navbar.jsx
@@ -8,6 +8,8 @@ import {
   ListItem
 } from '@chakra-ui/react'
 
+import { motion } from 'framer-motion'
+
 // icons
 import { TbArrowsRightLeft } from 'react-icons/tb';
 import { GiHamburgerMenu } from 'react-icons/gi';
@@ -47,78 +49,84 @@ const Navbar = () => {
 
   return (
     <div className="navbar__container">
-      <Flex color="#FFF" fontWeight="700" alignItems="center" justifyContent="space-around">
-        {/* left nav */}
-        <Flex alignItems="center">
-          <Box p="2" w="100px">
-            <Image src={logo} alt="logo" className="logo-image" />
-          </Box>
-          <Box p="2">
-            <Heading size="lg" className="navbar-title">Ranch PC</Heading>
-          </Box>
+      <motion.div
+        initial={{ opacity: 0, y: '2rem' }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1.2, delay: 0.5 }}
+      >
+        <Flex color="#FFF" fontWeight="700" alignItems="center" justifyContent="space-around">
+          {/* left nav */}
+          <Flex alignItems="center">
+            <Box p="2" w="100px">
+              <Image src={logo} alt="logo" className="logo-image" />
+            </Box>
+            <Box p="2">
+              <Heading size="lg" className="navbar-title">Ranch PC</Heading>
+            </Box>
+          </Flex>
+
+          {/* right nav */}
+          <Flex alignItems="center">
+            <List display={{ base: 'none', md: 'flex' }} className="gap-2">
+              <ListItem>Home</ListItem>
+              <ListItem>Services</ListItem>
+              <ListItem>About</ListItem>
+            </List>
+
+            {/* quote now button */}
+            <List display={{ base: 'none', md: 'flex' }} marginLeft="4" className="gap-2">
+              <Flex className="quote-btn">
+                <TbArrowsRightLeft fontSize={25} />
+                <span>Quote Now</span>
+              </Flex>
+            </List>
+
+            {/* hamburger menu button */}
+            <List display={{ base: 'flex', md: 'none'}}>
+              <GiHamburgerMenu
+                className="hamburger-menu"
+                fontSize={25}
+                onClick={handleMenuClick}
+              />
+            </List>
+          </Flex>
         </Flex>
 
-        {/* right nav */}
-        <Flex alignItems="center">
-          <List display={{ base: 'none', md: 'flex' }} className="gap-2">
-            <ListItem>Home</ListItem>
-            <ListItem>Services</ListItem>
-            <ListItem>About</ListItem>
-          </List>
+        {/* overlayed menu */}
+        {isMenuClicked && (
+          <div className="navbar__container-overlay">
+            <Flex alignItems="center" justifyContent="space-between">
+              <Box p="2" w="75px">
+                <Image src={logo} alt="logo" className="logo-overlay" />
+              </Box>
+              <IoIosClose
+                className="close-menu"
+                fontSize={45}
+                onClick={handleMenuClick}
+              />
+            </Flex>
 
-          {/* quote now button */}
-          <List display={{ base: 'none', md: 'flex' }} marginLeft="4" className="gap-2">
-            <Flex className="quote-btn">
+            <List className="list-overlay" spacing={4}>
+              <ListItem display="flex" alignItems="center" className="list-item-overlay">
+                <HiOutlineHome fontSize={25} />Home
+              </ListItem>
+              <ListItem display="flex" alignItems="center" className="list-item-overlay">
+                <MdOutlineMiscellaneousServices fontSize={25} /> Services
+              </ListItem>
+              <ListItem display="flex" alignItems="center" className="list-item-overlay">
+                <MdInfo fontSize={25} />About
+              </ListItem>
+            </List>
+
+            <hr className="my-2" />
+
+            <Flex className="quote-btn btn-overlay">
               <TbArrowsRightLeft fontSize={25} />
               <span>Quote Now</span>
             </Flex>
-          </List>
-
-          {/* hamburger menu button */}
-          <List display={{ base: 'flex', md: 'none'}}>
-            <GiHamburgerMenu
-              className="hamburger-menu"
-              fontSize={25}
-              onClick={handleMenuClick}
-            />
-          </List>
-        </Flex>
-      </Flex>
-
-      {/* overlayed menu */}
-      {isMenuClicked && (
-        <div className="navbar__container-overlay">
-          <Flex alignItems="center" justifyContent="space-between">
-            <Box p="2" w="75px">
-              <Image src={logo} alt="logo" className="logo-overlay" />
-            </Box>
-            <IoIosClose
-              className="close-menu"
-              fontSize={45}
-              onClick={handleMenuClick}
-            />
-          </Flex>
-
-          <List className="list-overlay" spacing={4}>
-            <ListItem display="flex" alignItems="center" className="list-item-overlay">
-              <HiOutlineHome fontSize={25} />Home
-            </ListItem>
-            <ListItem display="flex" alignItems="center" className="list-item-overlay">
-              <MdOutlineMiscellaneousServices fontSize={25} /> Services
-            </ListItem>
-            <ListItem display="flex" alignItems="center" className="list-item-overlay">
-              <MdInfo fontSize={25} />About
-            </ListItem>
-          </List>
-
-          <hr className="my-2" />
-
-          <Flex className="quote-btn btn-overlay">
-            <TbArrowsRightLeft fontSize={25} />
-            <span>Quote Now</span>
-          </Flex>
-        </div>
-      )}
+          </div>
+        )}
+      </motion.div>
     </div>
   )
 }
